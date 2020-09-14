937ff2fcd5c9f251b30e832d7b877ab492b12e2a
AlbinEsko <47393000+AlbinEsko@users.noreply.github.com>
2020-09-14 15:15:20 +0200

    Added obstacle avoidance
    Problem with walls and them flying along walls

--- G.A.R.A/Assets/Scripts/BOID.cs
+++ G.A.R.A/Assets/Scripts/BOID.cs
@@ -12,6 +12,7 @@ public class BOID : MonoBehaviour
     List<BOID> relevantBoids;
     [Header("Manuverability")]
     [SerializeField] float detectionRange = 5f;
+    float detectionRangeSqrd;
     [SerializeField] float maxSpeed = 5f;
     [SerializeField] [Range(0, 1)] float turningRate = 0.1f;
     [SerializeField] [Range(-1, 1)] float detection = -0.25f;
@@ -20,6 +21,8 @@ public class BOID : MonoBehaviour
     [SerializeField] float seperationWeight = 1f;
     [SerializeField] float allignmentWeight = 1f;
     [SerializeField] float cohesionWeight = 1f;
+    [SerializeField] float avoidWeight = 10f;
+    private float maxSteerForce = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +31,7 @@ public class BOID : MonoBehaviour
         rigidbody.velocity = transform.forward * maxSpeed / 2;
         allBoids = FindObjectsOfType<BOID>();
         relevantBoids = new List<BOID>();
+        detectionRangeSqrd = Mathf.Pow(detectionRange, 2);
     }
 
     // Update is called once per frame
@@ -35,12 +39,15 @@ public class BOID : MonoBehaviour
     {
         Detect();
         acceleration = CalcAcceleration();
+        acceleration += AvoidObstacle() * avoidWeight;
+        Debug.Log("pre: " + acceleration.magnitude);
+        acceleration = Vector3.ClampMagnitude(acceleration, maxSteerForce);
         rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, rigidbody.velocity + acceleration, turningRate);
-        if (rigidbody.velocity.magnitude > maxSpeed)
+        if (rigidbody.velocity.sqrMagnitude > Mathf.Pow(maxSpeed, 2))
             rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
-        else if (rigidbody.velocity.magnitude < maxSpeed / 10f)
+        else if (rigidbody.velocity.sqrMagnitude < Mathf.Pow(maxSpeed / 10f, 2))
             rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed / 10f;
-        transform.rotation = Quaternion.LookRotation(rigidbody.velocity.normalized);
+        transform.rotation = Quaternion.LookRotation(rigidbody.velocity);
     }
 
     private void OnDrawGizmosSelected()
@@ -57,7 +64,7 @@ public class BOID : MonoBehaviour
                 continue;
             Vector3 toOther = other.transform.position - transform.position;
             Vector3 normalizedToOther = Vector3.Normalize(toOther);
-            if (toOther.magnitude < detectionRange)
+            if (toOther.sqrMagnitude < detectionRangeSqrd)
                 if (Vector3.Dot(transform.forward, normalizedToOther) > detection)
                     relevantBoids.Add(other);
         }
@@ -68,18 +75,29 @@ public class BOID : MonoBehaviour
         Vector3 seperationVector = Vector3.zero;
         Vector3 allignmentVector = Vector3.zero;
         Vector3 cohesionVector = Vector3.zero;
+
         foreach (BOID other in relevantBoids)
         {
             seperationVector += CalcSeperation(other);
             allignmentVector += CalCAlignment(other);
             cohesionVector += CalcCohesion(other);
         }
+        Vector3 tempAcceleration;
         if (relevantBoids.Count > 0)
-            return (seperationVector * seperationWeight + allignmentVector * allignmentWeight + cohesionVector * cohesionWeight) / relevantBoids.Count;
+            tempAcceleration = (seperationVector * seperationWeight +
+                                allignmentVector * allignmentWeight +
+                                cohesionVector * cohesionWeight) /
+                                relevantBoids.Count;
         else
-            return transform.forward;
+            tempAcceleration = transform.forward;
+        return tempAcceleration;
     }
 
+    Vector3 SteerTowards(Vector3 vector)
+    {
+        Vector3 v = vector.normalized * maxSpeed - rigidbody.velocity;
+        return Vector3.ClampMagnitude(v, maxSteerForce);
+    }
 
     private Vector3 CalcSeperation(BOID other)
     {
@@ -94,5 +112,29 @@ public class BOID : MonoBehaviour
     private Vector3 CalcCohesion(BOID other)
     {
         return other.transform.position - transform.position;
+    }
+
+    private Vector3 AvoidObstacle()
+    {
+        RaycastHit hit;
+        if (!Physics.SphereCast(transform.position, 0.05f, transform.forward, out hit, detectionRange * 2, LayerMask.GetMask("Wall")))
+            return Vector3.zero;
+        Vector3[] obstAvoidRayDirs = BoidManager.CollisionRayDirections;
+        float shortestDistToObst = float.MaxValue;
+        for (int i = 0; i < obstAvoidRayDirs.Length; i++)
+        {
+            RaycastHit searchHit;
+            Vector3 dir = obstAvoidRayDirs[i];
+            Ray ray = new Ray(transform.position, dir);
+            if (!Physics.SphereCast(ray, 0.05f,out searchHit, detectionRange * 2, LayerMask.GetMask("Wall")))
+            {
+                Vector3 temp = dir / Mathf.Pow(shortestDistToObst, 2);
+                //Debug.Log(shortestDistToObst);
+                //Debug.Log(temp.magnitude);
+                return temp;
+            }
+            shortestDistToObst = Mathf.Min(shortestDistToObst, searchHit.distance);
+        }
+        return -transform.forward;
     }
 }
---
+++ G.A.R.A/Assets/Scripts/BoidManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour
+{
+    public static Vector3[] CollisionRayDirections { get; private set; }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetSampleDirections();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Returns well spaced directions on a sphere
+    /// </summary>
+    /// <param name="numSamples"></param>
+    /// <returns></returns>
+    // Credit to Sebastian Lague from programming adventures fame
+    public static void GetSampleDirections(int numSamples = 300)
+    {
+        int numPoints = numSamples;
+        CollisionRayDirections = new Vector3[numPoints];
+
+        float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
+        float angleIncrement = Mathf.PI * 2 * goldenRatio;
+
+        for (int i = 0; i < numPoints; i++)
+        {
+            float directionFraction = (float)i / numPoints;
+            float inclination = Mathf.Acos(1 - 2 * directionFraction);
+            float azimuth = angleIncrement * i;
+
+            float x = Mathf.Sin(inclination) * Mathf.Cos(azimuth);
+            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth);
+            float z = Mathf.Cos(inclination);
+
+            CollisionRayDirections[i] = new Vector3(x, y, z);
+        }
+    }
+}
