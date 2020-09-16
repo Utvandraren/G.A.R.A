ff633437db3fa9e19d6588c75476acbfc82b4774
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-09-16 23:20:01 +0200

    added beginning to the weapon system

---
+++ G.A.R.A/Assets/LaserWeapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class LaserWeapon : Weapon
+{
+    public override void Shoot()
+    {
+        base.Shoot();
+        // Draw line
+        //if line hit check tag of hit object
+        //If interactable object  -> call interactfunction inside it?
+    }
+
+    public override void UpdateDrawBack()
+    {
+        base.UpdateDrawBack();
+        Debug.DrawLine(transform.position, new Vector3(transform.position.x * 100f, 0, 0),Color.red);  // ta bort denna raden när spelet den inte behövs
+    }
+
+    
+}
---
+++ G.A.R.A/Assets/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]private GameObject firePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleShooterInput();
+        UpdateDrawBack();
+    }
+
+    void HandleShooterInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    public virtual void Shoot()
+    {
+
+    }
+
+    public virtual void UpdateDrawBack()
+    {
+
+    }
+}
