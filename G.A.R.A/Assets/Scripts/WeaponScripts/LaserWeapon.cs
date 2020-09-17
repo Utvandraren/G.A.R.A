caa0b98d2952b13252cb4f63cd2acb5979c60bc6
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-09-17 22:31:04 +0200

    added weapon manager and finished laser weapon prototype

--- G.A.R.A/Assets/Scripts/WeaponScripts/LaserWeapon.cs
+++ G.A.R.A/Assets/Scripts/WeaponScripts/LaserWeapon.cs
@@ -12,21 +12,21 @@ public class LaserWeapon : Weapon
     {
         base.Shoot();
 
-        //laserEffect.Play();
+        laserEffect.Play();
         //shootSound.Play();
 
         RaycastHit hit;
         Debug.LogFormat("Shots fired from: " + gameObject.ToString());
-        //Debug.DrawRay(firePoint.position, firePoint.forward, Color.red, 1f);  // ta bort denna raden när den inte behövs
 
-        if(Physics.Raycast(firePoint.position, firePoint.forward, out hit, 100f, 0))
+        if(Physics.Raycast(firePoint.position, firePoint.forward, out hit, 100f))
         {
             if(hit.transform.CompareTag("enemy"))
             {
                 //enemy.takedamage(Damage)    <-- change to the right function when it has been implemented
             }
-            else if(hit.transform.CompareTag("obj"))
+            else if(hit.transform.CompareTag("Player"))
             {
+                hit.transform.GetComponent<GunInteractTest>().Interact();
                 //object.interact             <-- change to the right function when it has been implemented
             }
         }
---
+++ G.A.R.A/Assets/Scripts/WeaponScripts/WeaponManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] GameObject laserWeapon;
+    [SerializeField] GameObject explosiveWeapon;
+    [SerializeField] GameObject TaserWeapon;
+
+
+
+    Weapons currentWeapon;
+    int mouseDelta;
+    int oldMouseDelta;
+
+    enum Weapons
+    {
+        Laser = 0,
+        Explosive = 1,
+        Taser = 2,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseDelta = 0;
+        oldMouseDelta = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleInput();
+    }
+
+    void handleInput()
+    {
+        mouseDelta += (int)Input.mouseScrollDelta.y;
+        mouseDelta = (int)Mathf.Clamp(mouseDelta, 0f, 2f);
+        Weapons weap = (Weapons)mouseDelta;
+
+        if(mouseDelta != oldMouseDelta)
+        {
+            ChangeWeapon(weap);
+            oldMouseDelta = mouseDelta;
+        }
+    }
+
+    void ChangeWeapon(Weapons weaponToChange)
+    {
+        switch (weaponToChange)
+        {
+            case Weapons.Laser:
+                laserWeapon.SetActive(true);
+                explosiveWeapon.SetActive(false);
+                TaserWeapon.SetActive(false);
+                break;
+
+            case Weapons.Explosive:
+                explosiveWeapon.SetActive(true);
+                laserWeapon.SetActive(false);
+                TaserWeapon.SetActive(false);
+                break;
+
+            case Weapons.Taser:
+                TaserWeapon.SetActive(true);
+                explosiveWeapon.SetActive(false);
+                laserWeapon.SetActive(false);
+                break;
+            default:
+                //
+                break;
+        }
+    }
+}
