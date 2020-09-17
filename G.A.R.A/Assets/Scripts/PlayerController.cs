17e36fd9c714a51a162537cee03fa6d1eb77f654
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2020-09-17 21:19:04 +0200

    Movement and rotation added
    Movement: W+A+S+D+LeftCtrl+Space
    Rotation: Q+E
    
    Right now rotation is not done physically, the current iteration mimics a physical feeling however which is close enough to what we want in my (David) opinion.
    
    Rigidbody drag is used to stop the player once they release all buttons. It was harder than I expected to get it to work and look smooth by using only AddForce to counteract movement. This might be something we could look into should it prove necessary.

--- G.A.R.A/Assets/Scripts/PlayerController.cs
+++ G.A.R.A/Assets/Scripts/PlayerController.cs
@@ -5,132 +5,175 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    private float maxSpeed;
+    private float maxSpeed; //Will use later hopefully
 
+    //Rigidbody and physics related
+    private Rigidbody rb;
     [SerializeField] private float thrustForce;
-    [SerializeField] private float stoppingThrustForce;
+    [SerializeField] private float dampeningThrustForceMax;
+    private float dampeningThrustForce;
+    private float dragCoef = .8f;
+    private float angularDragCoef = 10;
 
-    [SerializeField, Range(.1f, 2f)] private float mouseSensitivityMultiplier = 1f;
+    float currentRollRate = 0.0f;
+    float maxRollRate = 5.0f;
+    [SerializeField] private float rollLerpTime = 2f;
 
-        class CameraState
+    [SerializeField, Range(.1f, 2f)] private float mouseSensitivityMultiplier = 1f; //Might move this later to a manager type script
+
+    class CameraState
+    {
+        public float yaw;
+        public float pitch;
+        public float roll;
+        public float x;
+        public float y;
+        public float z;
+
+        public void SetFromTransform(Transform t)
         {
-            public float yaw;
-            public float pitch;
-            public float roll;
-            public float x;
-            public float y;
-            public float z;
-
-            public void SetFromTransform(Transform t)
-            {
-                pitch = t.eulerAngles.x;
-                yaw = t.eulerAngles.y;
-                roll = t.eulerAngles.z;
-                x = t.position.x;
-                y = t.position.y;
-                z = t.position.z;
-            }
-
-            public void Translate(Vector3 translation)
-            {
-                Vector3 rotatedTranslation = Quaternion.Euler(pitch, yaw, roll) * translation;
-
-                x += rotatedTranslation.x;
-                y += rotatedTranslation.y;
-                z += rotatedTranslation.z;
-            }
-
-            public void LerpTowards(CameraState target, float positionLerpPct, float rotationLerpPct)
-            {
-                yaw = Mathf.Lerp(yaw, target.yaw, rotationLerpPct);
-                pitch = Mathf.Lerp(pitch, target.pitch, rotationLerpPct);
-                roll = Mathf.Lerp(roll, target.roll, rotationLerpPct);
-
-                x = Mathf.Lerp(x, target.x, positionLerpPct);
-                y = Mathf.Lerp(y, target.y, positionLerpPct);
-                z = Mathf.Lerp(z, target.z, positionLerpPct);
-            }
-
-            public void UpdateTransform(CameraState target, Transform t)
-            {
-                //t.eulerAngles = new Vector3(pitch, yaw, roll);
-                t.Rotate(new Vector3(target.pitch, target.yaw, target.roll), Space.Self);
-                t.position = new Vector3(x, y, z);
-            }
+            pitch = t.eulerAngles.x;
+            yaw = t.eulerAngles.y;
+            roll = t.eulerAngles.z;
+            x = t.position.x;
+            y = t.position.y;
+            z = t.position.z;
         }
 
-        CameraState m_TargetCameraState = new CameraState();
-        CameraState m_InterpolatingCameraState = new CameraState();
-
-        [Header("Movement Settings")]
-        [Tooltip("Whether or not to invert our Y axis for mouse input to rotation.")]
-        public bool invertY = false;
-
-        void OnEnable()
+        public void Translate(Vector3 translation)
         {
-            m_TargetCameraState.SetFromTransform(transform);
-            m_InterpolatingCameraState.SetFromTransform(transform);
+            Vector3 rotatedTranslation = Quaternion.Euler(pitch, yaw, roll) * translation;
+
+            x += rotatedTranslation.x;
+            y += rotatedTranslation.y;
+            z += rotatedTranslation.z;
         }
 
-        Vector3 GetInputTranslationDirection()
+        public void UpdateRotation(CameraState target, Transform t)
         {
-            Vector3 direction = new Vector3();
-            if (Input.GetKey(KeyCode.W))
-            {
-                direction += Vector3.forward;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                direction += Vector3.back;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                direction += Vector3.left;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                direction += Vector3.right;
-            }
-            if (Input.GetKey(KeyCode.Q))
-            {
-                direction += Vector3.down;
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                direction += Vector3.up;
-            }
-            return direction;
+            //t.eulerAngles = new Vector3(pitch, yaw, roll);
+            t.Rotate(new Vector3(target.pitch, target.yaw, target.roll), Space.Self);
         }
+    }
 
-    void Update()
+    private CameraState m_TargetCameraState = new CameraState();
+    private CameraState m_CameraState = new CameraState();
+
+    [Header("Movement Settings")]
+    [Tooltip("Whether or not to invert our Y axis for mouse input to rotation.")]
+    public bool invertY = false;
+
+    private Vector3 GetInputTranslationDirection()
+    {
+        Vector3 direction = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += gameObject.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += -gameObject.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += -gameObject.transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += gameObject.transform.right;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            direction += -gameObject.transform.up;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            direction += gameObject.transform.up;
+        }
+        return direction;
+    }
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.drag = dragCoef;
+        rb.angularDrag = angularDragCoef;
+        dampeningThrustForce = dampeningThrustForceMax;
+    }
+
+    private void OnEnable()
+    {
+        m_TargetCameraState.SetFromTransform(transform);
+        m_CameraState.SetFromTransform(transform);
+    }
+
+    private void Update()
     {
         Vector3 translation = Vector3.zero;
 
         #if ENABLE_LEGACY_INPUT_MANAGER
 
-
-        // Rotation
-
+        // Mouse Rotation
         var mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
-
         m_TargetCameraState.yaw = mouseMovement.x * mouseSensitivityMultiplier;
         m_TargetCameraState.pitch = mouseMovement.y * mouseSensitivityMultiplier;
 
+        //Keyboard rotation
+        if (Input.GetKey(KeyCode.Q))
+        {
+            currentRollRate = Mathf.Lerp(currentRollRate, maxRollRate, 1f - Mathf.Exp((Mathf.Log(1f - 0.8f) / rollLerpTime) * Time.deltaTime));
+            transform.Rotate(new Vector3(0, 0, currentRollRate));
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            currentRollRate = Mathf.Lerp(currentRollRate, -maxRollRate, 1f - Mathf.Exp((Mathf.Log(1f - 0.8f) / rollLerpTime) * Time.deltaTime));
+            transform.Rotate(new Vector3(0, 0, currentRollRate));
+        }
+        else
+        {
+            currentRollRate = Mathf.Lerp(currentRollRate, 0, 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / (rollLerpTime)) * Time.deltaTime));
+            transform.Rotate(new Vector3(0, 0, currentRollRate));
+        }
+
+
         // Translation
         translation = GetInputTranslationDirection() * Time.deltaTime;
-
         #elif USE_INPUT_SYSTEM
             // TODO: make the new input system work
         #endif
 
         m_TargetCameraState.Translate(translation);
+        m_CameraState.UpdateRotation(m_TargetCameraState, transform);
+    }
 
-        //// Framerate-independent interpolation
-        //// Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
-        //var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
-        //var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
-        //m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
 
-        m_InterpolatingCameraState.UpdateTransform(m_TargetCameraState, transform);
+    //Everything physics related must be added here, not in Update()
+    private void FixedUpdate()
+    {
+        Vector3 translation = Vector3.zero;
+        translation = GetInputTranslationDirection() * Time.deltaTime;
+
+        //Movement and Thrust
+        if (translation != Vector3.zero)
+        {
+            Vector3 direction = translation.normalized;
+            direction *= thrustForce;
+            rb.AddForce(direction * Time.deltaTime, ForceMode.Force);
+            dampeningThrustForce = dampeningThrustForceMax;
+        }
+
+        //Doesn't wörk, might use later for now, use rigidbody drag to achieve similar effect
+        //else
+        //{
+        //    var velocityLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.8f) / stopLerpTime) * Time.deltaTime);
+        //    Vector3 velocityVector = rb.velocity.normalized;
+        //    Vector3 dampeningVector = -velocityVector;
+        //    rb.AddForce(dampeningVector * dampeningThrustForce, ForceMode.Force);
+        //}
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 250, 100), currentRollRate.ToString());
     }
 }
