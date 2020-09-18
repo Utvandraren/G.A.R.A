e2c33233182974c2764cd2cfebb766c30d8738c5
oporenius <oporenius@gmail.com>
2020-09-18 14:06:39 +0200

    Text dialogue

---
+++ G.A.R.A/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+}
