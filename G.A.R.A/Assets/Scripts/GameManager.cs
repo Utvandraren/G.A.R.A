1f3ca7197b750ddf11257baa58ed70dfcbed745f
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-09-21 17:04:19 +0200

    Finished loading and unloading for gamemanager

--- G.A.R.A/Assets/Scripts/GameManager.cs
+++ G.A.R.A/Assets/Scripts/GameManager.cs
@@ -5,21 +5,96 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    private string currentScene;
+    public GameObject[] systemPrefabs;
 
+    private string currentLevel;
+    private List<AsyncOperation> loadOperations;
+    private List<GameObject> instancedSystemPrefabs;
+    
 
-    public void LoadScene(string levelName)
+    void Start()
     {
+        DontDestroyOnLoad(gameObject);
+        loadOperations = new List<AsyncOperation>();
+        instancedSystemPrefabs = new List<GameObject>();
 
+        InstantiateSystemPrefabs();
+
+        //LoadLevel("Start");
+    }
+
+    void InstantiateSystemPrefabs()
+    {
+        GameObject prefabInstance;
+        for (int i = 0; i < systemPrefabs.Length; i++)
+        {
+            prefabInstance = Instantiate(systemPrefabs[i]);
+            instancedSystemPrefabs.Add(prefabInstance);
+        }
+    }
+
+    void OnLoadOperationComplete(AsyncOperation ao)
+    {
+        if (loadOperations.Contains(ao))
+        {
+            loadOperations.Remove(ao);
+            //transition between scenes here
+        }
+        Debug.Log("Load complete");
+    }
+
+    void OnUnloadOperationComplete(AsyncOperation ao)
+    {
+        Debug.Log("Unload complete");
+    }
+
+
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+
+        if(ao == null)
+        {
+            Debug.LogError("[Gamemanager] unable to load level " + levelName);
+            return;
+        }
+
+        ao.completed += OnLoadOperationComplete;
+        loadOperations.Add(ao);
+        currentLevel = levelName;
+    }
+
+    public void UnloadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+
+        if (ao == null)
+        {
+            Debug.LogError("[Gamemanager] unable to unload level " + levelName);
+            return;
+        }
+
+        ao.completed += OnUnloadOperationComplete;
     }
 
     public void GameOver()
     {
-        //Set UI. GameOVer
+        //UIManager.setLoseUI;
     }
 
     public void ResetGame()
     {
-        
+        //LoadLevel("MainMeny");
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        for (int i = 0; i < instancedSystemPrefabs.Count; i++)
+        {
+            Destroy(instancedSystemPrefabs[i]);
+        }
+
+        instancedSystemPrefabs.Clear();
     }
 }
