4ecfb01044db2eee70b4d48954bd605d87751584
AlbinEsko <47393000+AlbinEsko@users.noreply.github.com>
2020-09-25 15:42:15 +0200

    Fixed Shielded enemy

---
+++ G.A.R.A/Assets/Scripts/ShielderStats.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Stats for bubble Shields
+    /// </summary>
+    class ShielderStats : EnemyStats
+    {
+        public int shieldHealth = 50;
+        public GameObject shield;
+        public override void TakeDamage(SciptableAttackObj attack)
+        {
+            if (attack.element == SciptableAttackObj.WeaponElement.Electricity)
+            {
+                shieldHealth = 0;
+                return;
+            }
+            if (shieldHealth <= 0)
+                base.TakeDamage(attack);
+            else
+                DamageShield(attack);
+        }
+        private void DamageShield(SciptableAttackObj attack)
+        {
+            shieldHealth -= attack.damage;
+            shieldHealth = Mathf.Max(0, shieldHealth);
+            if (shieldHealth <= 0)
+                shield.SetActive(false);
+        }
+    }
+}
