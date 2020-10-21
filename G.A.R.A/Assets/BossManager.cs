61e30936093cd63ed5398ac7a9597b65941b4576
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-10-21 21:45:05 +0200

    added beginning to boss enemy

---
+++ G.A.R.A/Assets/BossManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    BossPhases currentPhase = BossPhases.ShieldPhase;
+    Animator animator;
+
+    public enum BossPhases
+    {
+        ShieldPhase = 0,
+        SwarmPhase = 1,
+        TentaclePhase = 2,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TransitionToNextPhase();
+        animator = GetComponent<Animator>();
+    }
+
+    public void TransitionToNextPhase()
+    {
+        switch (currentPhase)
+        {
+            case BossPhases.ShieldPhase:
+                StartShieldPhase();
+                currentPhase++;
+                break;
+
+            case BossPhases.SwarmPhase:
+                StartSwarmPhase();
+                currentPhase++;
+                break;
+
+            case BossPhases.TentaclePhase:
+                StartTentaclePhase();
+                break;
+
+            default:
+                //
+                break;
+        }
+    }
+
+    void StartShieldPhase()
+    {
+
+    }
+
+    void StartSwarmPhase()
+    {
+
+    }
+
+    void StartTentaclePhase()
+    {
+
+    }
+}
---
+++ G.A.R.A/Assets/Scripts/Character/Enemy/BossStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStats : Stats
+{
+    public SciptableIntObj bossHealth;
+
+    int treshold;
+
+    protected override void Start()
+    {
+        treshold = bossHealth.startValue / 3;
+    }
+
+    //
+    public override void TakeDamage(SciptableAttackObj attack)
+    {
+        bossHealth.value -= attack.damage;
+        treshold -= attack.damage;
+
+        if (bossHealth.value <= 0)
+        {
+            Die();
+        }
+        else if(treshold <= 0)
+        {
+            treshold = bossHealth.startValue / 3;
+            GetComponent<BossManager>().TransitionToNextPhase();
+        }
+    }
+
+    public override void Die()
+    {
+        //Gamemanager.winGame();
+        Debug.Log("Boss dead!!!");
+    }
+}
