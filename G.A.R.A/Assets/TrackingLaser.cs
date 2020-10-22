7dfe3c55b9b6bab9f713d404d69376bf8af8fcac
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-10-22 13:35:47 +0200

    added rotating trackinglaser to boss

---
+++ G.A.R.A/Assets/TrackingLaser.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingLaser : Weapon
+{
+    [SerializeField] private float trackingTime = 0f;
+    [SerializeField] private float attackDuration = 0f;
+    [SerializeField] private GameObject laserBeam;
+
+
+    private GameObject target;
+    private Transform currentTransform;
+
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        base.Start();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentDirection = target.transform.position - transform.position;
+        //transform.rotation = Quaternion.LookRotation(currentDirection);
+        Quaternion neededRotation = Quaternion.LookRotation(currentDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, neededRotation, Time.deltaTime * trackingTime);
+    } 
+
+    public override void Shoot()
+    {
+        base.Shoot();
+        StartCoroutine(ActivateLaser());
+    }
+
+    public IEnumerator ActivateLaser()
+    {
+        laserBeam.SetActive(true);
+        //PLayVisualEffects
+        yield return new WaitForSeconds(attackDuration);
+        laserBeam.SetActive(false);
+
+    }
+}
