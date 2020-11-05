7154ec18a4f949791f9a7d4a80002e968f4065b1
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2020-11-05 21:15:27 +0100

    Fixed issues with main menu, started working on pause menu

---
+++ G.A.R.A/Assets/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    [SerializeField] private GameObject pauseMenuObject;
+
+    [SerializeField] private Options options;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause game"))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Pause();
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+
+    public void BackToMain()
+    {
+        GameManager.Instance.LoadScene("MainMenu");
+    }
+
+    public void ShowOptions()
+    {
+        options.OpenOptions();
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Why are you quitting!?");
+        Application.Quit();
+    }
+
+    public void Resume()
+    {
+        pauseMenuObject.SetActive(false);
+        //Insert animation later?
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuObject.SetActive(true);
+        //Insert animation later?
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
