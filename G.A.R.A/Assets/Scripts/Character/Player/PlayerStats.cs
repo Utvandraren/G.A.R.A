23350bc4a57e3fec3ee19b19147b1101922b516e
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2020-11-10 17:43:09 +0100

    Fixed a few bugs with sprinting and tweaked player movement

--- G.A.R.A/Assets/Scripts/Character/Player/PlayerStats.cs
+++ G.A.R.A/Assets/Scripts/Character/Player/PlayerStats.cs
@@ -6,39 +6,80 @@ using UnityEngine.UI;
 
 public class PlayerStats : Stats
 {
-    public int shield;
+    public float shield;
+    public float sprint;
     [SerializeField] public int maxShield;
-    [SerializeField] float shieldRechargeDelay;
-    [SerializeField] int shieldRechargeRate;
-    float timer;
+    [SerializeField] public int maxSprint;
+    [SerializeField] private float shieldRechargeDelay;
+    [SerializeField] private float shieldRechargeRate;
+    [SerializeField] private float sprintRechargeDelay;
+    [SerializeField] private float sprintRechargeRate;
+    [SerializeField] private float sprintUsageRate;
+
+    private float shieldTimer;
+    private float sprintTimer;
+
+    private float sprintRegenThreshold = 0.15f;
+    public bool canSprint { get; private set; }
+
+    private PlayerController player;
 
     protected override void Start()
     {
         base.Start();
         shield = maxShield;
+        player = GetComponent<PlayerController>();
     }
 
     private void Update()
     {
         if (shield == maxShield)
         {
-            timer = 0;
-            return;
+            shieldTimer = 0;
         }
-        if (timer >= shieldRechargeDelay)
+        if (shieldTimer >= shieldRechargeDelay)
             RechargeShield();
         else
-            timer += Time.deltaTime;
+            shieldTimer += Time.deltaTime;
+
+
+        if(player.isSprinting)
+        {
+            DrainSprint();
+            sprintTimer = 0;
+        }
+        if (sprintTimer >= sprintRechargeDelay && !player.isSprinting)
+            RechargeSprint();
+        else sprintTimer += Time.deltaTime;
+
+        if (sprint / maxSprint <= sprintRegenThreshold && !player.isSprinting)
+        {
+            canSprint = false;
+        }
+        else canSprint = true;
     }
 
     private void RechargeShield()
     {
-        shield += shieldRechargeRate;
+        shield += shieldRechargeRate * Time.deltaTime;
         shield = Mathf.Min(shield, maxShield);
     }
+
+    private void RechargeSprint()
+    {
+        sprint += sprintRechargeRate * Time.deltaTime;
+        sprint = Mathf.Min(sprint, maxSprint);
+    }
+
+    private void DrainSprint()
+    {
+        sprint -= sprintUsageRate * Time.deltaTime;
+        sprint = Mathf.Min(sprint, maxSprint);
+    }
+
     public override void TakeDamage(SciptableAttackObj attack)
     {
-        timer = 0;
+        shieldTimer = 0;
         if(shield == 0)
             base.TakeDamage(attack);
         else
