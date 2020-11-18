1a7f075717537863153b0e5ee962cc76da41c56e
ktgktg <ktgktg@hotmail.com>
2020-11-18 17:17:45 +0100

    Building - Skybox with revolve added (again)

---
+++ G.A.R.A/Assets/Revolve.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolve : MonoBehaviour
+{
+
+    [SerializeField, Range(0F, 1F)] private float revolveFactor;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * revolveFactor);
+    }
+}
