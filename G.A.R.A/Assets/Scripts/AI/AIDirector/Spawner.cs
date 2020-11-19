ba93db18e9f504ff913e27d5241e2b7afd9813c9
AlbinEsko <47393000+AlbinEsko@users.noreply.github.com>
2020-11-19 15:43:02 +0100

    Basic AID lvl2 skeleton done

--- G.A.R.A/Assets/Scripts/AI/AIDirector/Spawner.cs
+++ G.A.R.A/Assets/Scripts/AI/AIDirector/Spawner.cs
@@ -10,7 +10,6 @@ public class Spawner : MonoBehaviour
         WALL
     }
     public Type type;
-    public int index;
     public void Spawn(GameObject gameObject)
     {
         Instantiate(gameObject, transform.position + Random.insideUnitSphere, transform.rotation);
