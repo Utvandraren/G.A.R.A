24408301a18d93dd5f793dc825a1b2553ca0d65b
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-11-20 11:14:14 +0100

    added new winprompt

---
+++ G.A.R.A/Assets/OnKeyPressedReturnToMeny.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnKeyPressedReturnToMeny : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            GameManager.Instance.ReturnToMain();
+        }
+    }
+
+  
+}
