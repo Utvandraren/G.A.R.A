1e3c91674ecf5c3a8815c4ea24fa491cdff37222
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-11-30 19:01:13 +0100

    improved introtimeline with audio and more details

---
+++ G.A.R.A/Assets/AutomaticLevelTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticLevelTransition : MonoBehaviour
+{
+
+    [SerializeField] private string LevelToGoTo;
+
+    public void LoadScene()
+    {
+        Debug.Log("new scene loaded!!!!");
+        GameManager.Instance.GoToNextLevel("LevelToGoTo");
+    }
+
+    
+    void OnEnable()
+    {
+        LoadScene();
+    }
+}
