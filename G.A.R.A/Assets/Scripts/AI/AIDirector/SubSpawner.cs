10c6a165377c02487a9231f367ac38f312a00fdb
AlbinEsko <47393000+AlbinEsko@users.noreply.github.com>
2020-11-30 13:36:30 +0100

    started subspawner

---
+++ G.A.R.A/Assets/Scripts/AI/AIDirector/SubSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class SubSpawner : MonoBehaviour
+{
+    public void Spawn(GameObject gameObject)
+    {
+        Instantiate(gameObject, transform.position + Random.insideUnitSphere, transform.rotation);
+    }
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, 1);
+        //Gizmos.DrawWireSphere(transform.position, 5);
+    }
+}
