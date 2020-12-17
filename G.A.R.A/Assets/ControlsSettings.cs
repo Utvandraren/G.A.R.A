2cbc7603a164ab4ed53478c1628ee4c478d4e187
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2020-12-17 01:33:22 +0100

    Added mouse sensitivity settings to the controls panel fixed controls not showing

---
+++ G.A.R.A/Assets/ControlsSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsSettings : MonoBehaviour
+{
+
+    [SerializeField] private Slider sensitivitySlider;
+    [SerializeField] private TMP_Text sensitivityVal;
+    private PlayerController pc;
+
+    private void Start()
+    {
+        pc = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        sensitivityVal.text = sensitivitySlider.
+    }
+
+    public void UpdateSensitivity()
+    {
+        string desiredDisplayText = (sensitivitySlider.value * 100).ToString();
+        if (desiredDisplayText.Substring(0, 4) == "99,8")
+        {
+            desiredDisplayText = "100,0";
+        }
+        else
+        {
+            desiredDisplayText = (sensitivitySlider.value * 100).ToString().Substring(0, 4);
+        }
+        sensitivityVal.text = desiredDisplayText;
+        sensitivityVal.text += "%";
+        pc.SetSensitivity(sensitivitySlider.value);
+    }
+}
