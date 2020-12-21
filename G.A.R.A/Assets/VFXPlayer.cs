7c4f1d58ba16872bcebeeb7557b5eb2c84b7eed2
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2020-12-21 01:02:47 +0100

    Fixed problem with switching weapon directly after firing with the explosive weapon
    Muzzle flash would play at the wrong time, this is now fixed using a small script called VFXPlayer and calling the TriggerEffects() method in an animation event.

---
+++ G.A.R.A/Assets/VFXPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+
+/// <summary>
+/// This class is used to play visual effects that aren't supposed to loop but you want to trigger in an animation.
+/// Mostly used for muzzle flashes and similar effects created using visual effect graphs.
+/// If there is a better way to do it, I don't know it.
+/// </summary>
+public class VFXPlayer : MonoBehaviour
+{
+    [SerializeField] private VisualEffect vfx;
+
+    public void TriggerEffect()
+    {
+        vfx.Play();
+    }
+}
